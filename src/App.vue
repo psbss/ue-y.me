432a76380bd939fdfeff405be55cd428c63a8c45
uetyo <uehara.yuki@dely.jp>
2022-06-09 07:20:04 +0900

    fix: box-shadow

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
   max-width: 20rem;
   margin: auto;
   border-radius: 0.5rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
   overflow: hidden;
   background: $color_white;
 }
