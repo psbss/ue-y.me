b8cc599cb36d61916cd049ef60b9f0ba4046636a
uetyo <uehara.yuki@dely.jp>
2022-06-08 22:04:35 +0900

    add: component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <profile />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import profile from "./components/Profile.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    profile,
   },
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
+    "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  width: 100%;
+  max-width: 20rem;
+  margin: auto;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  background: $color_white;
 }
 </style>
